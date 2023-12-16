d062a0e200c9c5e02fc2f38fea653ae92f8baf1a
jcfr4n <52176879+jcfr4n@users.noreply.github.com>
2023-12-16 22:36:03 +0000

    2da version

--- raspberry_lamp.sh
+++ raspberry_lamp.sh
@@ -1,36 +1,69 @@
 #!/bin/bash
 
+# Limpiamos la pantalla
+clear
+
+# Verificamos si el script se está ejecutando como root
 if [ "$(whoami)" != "root" ]; then
-    echo "Run script as ROOT please. (sudo !!)"
+    echo "Ejecuta el script como ROOT. (sudo !!)"
     exit
 fi
 
-## Instalamos apache
-apt-get install apache2 -y
+# Informamos al usuario que estamos actualizando el sistema
+echo "Actualizando el sistema..."
+apt update
+apt upgrade -y
+echo "Actualización del sistema completada."
 
-## Instalamos PHP
-apt-get install php php-mysql libapache2-mod-php -y
+# Pedimos el nombre del usuario
+read -p "Ingresa el nombre de usuario para la configuración del servidor web: " username
 
-## Instalar MariaDB
-apt-get install mariadb-server mariadb-client -y
+# Instalamos apache y proporcionamos un mensaje informativo
+echo "Instalando Apache..."
+apt install apache2 -y
+echo "Apache instalado."
 
-## Securizamos la instalación de MariDB
+# Instalamos PHP y proporcionamos un mensaje informativo
+echo "Instalando PHP..."
+apt install php php-mysql libapache2-mod-php -y
+echo "PHP instalado."
+
+# Instalamos MariaDB y proporcionamos un mensaje informativo
+echo "Instalando MariaDB..."
+apt install mariadb-server mariadb-client -y
+echo "MariaDB instalado."
+
+# Securizamos la instalación de MariaDB
+echo "Asegurando la instalación de MariaDB..."
 mysql_secure_installation
+echo "Instalación de MariaDB asegurada."
 
-## Instalamos phpmyadmin
-apt-get install phpmyadmin -y
+# Instalamos phpMyAdmin y proporcionamos un mensaje informativo
+echo "Instalando phpMyAdmin..."
+apt install phpmyadmin -y
+echo "phpMyAdmin instalado."
 
-## Cambiamos el propietario del directorio /html que es en donde irán nuestras páginas, se agrega el usuario pi al grupo
-## y se cambian los permisos de lectura y escritura
+# Instalamos Composer y proporcionamos un mensaje informativo
+echo "Instalando Composer..."
+apt install composer -y
+echo "Composer instalado."
+
+# Instalamos Node.js y npm y proporcionamos un mensaje informativo
+echo "Instalando Node.js y npm..."
+apt install nodejs npm -y
+echo "Node.js y npm instalados."
+
+# Cambiamos el propietario del directorio /html y proporcionamos un mensaje informativo
+echo "Configurando permisos y propietario..."
 chown -R www-data:www-data /var/www/html
-usermod -g www-data pi
+usermod -g www-data $username
 chmod -R 777 /var/www
+echo "Permisos y propietario configurados."
+
+# Reiniciamos Apache para que corra phpMyAdmin
+echo "Reiniciando Apache..."
+systemctl restart apache2
+echo "Apache reiniciado."
 
 echo
-echo
-echo
-
-## El siguiente comando reinicia apache2 para que corra phpmyadmin
-service apache2 reload
-
-echo "Instalación finalizada!!!!"
+echo "¡Instalación completada!"
